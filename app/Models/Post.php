30bce8ce3f0cf10f735b683ddc2b87f253ed2339
Alita193040125 <193040125.alita@mail.unpas.ac.id>
2021-11-20 19:02:26 +0700

    menyelesaikan video ke-18 | Create Post Form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
+    use Sluggable;
 
     // protected $fillable = ['title', 'excerpt', 'body']; //yang ada dalam array harus diisi
     protected $guarded = ['id']; //yang tidak boleh diisi tapi sisanya harus dijaga
@@ -49,5 +51,12 @@ class Post extends Model
         return 'slug';
     }
 
-
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
